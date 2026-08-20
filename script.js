4b2b239c01830f713d3f16120e84e00b659fac40
mymcworld3-droid <mymcworld3@gmail.com>
2026-08-20 14:52:14 +0800

    Update weapon image format from jpeg to png

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const sprites = {
 sprites.body.src = "asset/body.png";
 sprites.eyes.src = "asset/eyes.png";
 sprites.hand.src = "asset/hand.png";
-sprites.weaponMoyan.src = "asset/weapons/魔炎熾魂刀.jpeg";
+sprites.weaponMoyan.src = "asset/weapons/魔炎熾魂刀.png";
 sprites.skillMoyan.src = "asset/skills/魔炎熾魂刀技能.jpeg";
 
 /* =========================
